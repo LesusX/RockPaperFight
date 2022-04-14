493e635aa81ba6a2712708451da7fe6bdebe6096
7eventh <55671529+7eventh@users.noreply.github.com>
2022-04-14 15:26:37 +0200

    Update client.py

--- client.py
+++ client.py
@@ -2,37 +2,25 @@ import pygame
 from network import Network
 import pickle
 import sys 
-pygame.font.init()
+from buttons import * 
+from menu import * 
+import test 
 
-width = 700
-height = 700
+pygame.init()
+pygame.font.init()
+# ---------------------------------Set game variables----------------------------
+width = 1200
+height = 720
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
+btns = [SimpleButton("Rock", 50, 500, (0,0,0)), SimpleButton("Scissors", 250, 500, (255,0,0)), SimpleButton("Paper", 450, 500, (0,255,0))]
+BG = pygame.image.load("assets/menu_pic.png")
 
-class Button:
-    def __init__(self, text, x, y, color):
-        self.text = text
-        self.x = x
-        self.y = y
-        self.color = color
-        self.width = 150
-        self.height = 100
+# ------------------------------Game functions-----------------------------------
 
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-        font = pygame.font.SysFont("comicsans", 40)
-        text = font.render(self.text, 1, (255,255,255))
-        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
-
-    def click(self, pos):
-        x1 = pos[0]
-        y1 = pos[1]
-        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
-            return True
-        else:
-            return False
-
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
 
 def redrawWindow(win, game, p):
     win.fill((128,128,128))
@@ -81,8 +69,9 @@ def redrawWindow(win, game, p):
 
     pygame.display.update()
 
-
-btns = [Button("Rock", 50, 500, (0,0,0)), Button("Scissors", 250, 500, (255,0,0)), Button("Paper", 450, 500, (0,255,0))]
+'''
+main is responsible for the online part of the game and 
+'''
 def main():
     run = True
     clock = pygame.time.Clock()
@@ -139,7 +128,8 @@ def main():
 
         redrawWindow(win, game, player)
 
-def menu_screen():
+
+def play_online_win():  # TODO: delete this in the end 
     run = True
     clock = pygame.time.Clock()
 
@@ -158,8 +148,120 @@ def menu_screen():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 run = False
-
     main()
 
+
+def play_offline():
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        win.fill("black")
+
+        PLAY_TEXT = get_font(45).render("This is the PLAY win.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        win.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = BaseButton(image=None, pos=(640, 390), 
+                            text_input="Play ofline", font=get_font(50), base_color="White", hovering_color="Green")
+
+        ON_BACK = BaseButton(image=None, pos=(640, 480), 
+                            text_input="Play online", font=get_font(50), base_color="White", hovering_color="Green")
+
+        SC_BACK = BaseButton(image=None, pos=(640, 560), 
+                            text_input="BACK to main menu", font=get_font(50), base_color="White", hovering_color="Green")
+        
+        
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(win)
+        SC_BACK.changeColor(PLAY_MOUSE_POS)
+        SC_BACK.update(win)
+        ON_BACK.changeColor(PLAY_MOUSE_POS)
+        ON_BACK.update(win)        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    test.gameLoop()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if ON_BACK.checkForInput(PLAY_MOUSE_POS):
+                    play_online_win() # TODO: replace with main()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if SC_BACK.checkForInput(PLAY_MOUSE_POS):
+                    return False
+        pygame.display.update()
+    
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        win.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS win.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        win.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = BaseButton(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(win)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    return False  
+
+        pygame.display.update()
+
+
+def main_menu():
+    run = True 
+
+    while run:
+        win.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#000000")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = BaseButton(image=None, pos=(640, 250), 
+                            text_input="PLAY", font=get_font(75), base_color="#FF1212", hovering_color="#D52222")
+        OPTIONS_BUTTON = BaseButton(image=None, pos=(640, 400), 
+                            text_input="OPTIONS", font=get_font(75), base_color="#FF1212", hovering_color="#D52222")
+        QUIT_BUTTON = BaseButton(image=None, pos=(640, 550), 
+                            text_input="QUIT", font=get_font(75), base_color="#FF1212", hovering_color="#D52222")
+
+        win.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(win)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play_offline() 
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
+
+
+
 while True:
-    menu_screen()
+    main_menu()
