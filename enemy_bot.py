4cf703a85be54675126eda875438c2d9f7a024a4
7eventh <55671529+7eventh@users.noreply.github.com>
2022-04-26 13:47:25 +0200

    Update enemy_bot.py

--- enemy_bot.py
+++ enemy_bot.py
@@ -10,22 +10,13 @@ class Enemy:
     def __init__(self):
         names = ["Mark", "John", "Steven", "Roger", "Joseph", "JOJO"]
         self.name = random.choice(names)
-        self.can_move = True 
         self.bot_move = []
 
     def set_bot_move(self):
         moves = ["Rock", "Paper", "Scissors"]
-        if self.can_move == True:
-            moves = ["Rock", "Paper", "Scissors"]
-            self.bot_move.append(random.choice(moves))
-            return self.bot_move
-        else:
-            return str("You cant move yet")
-
-    def clean_list(self):
-        for i in self.bot_move[:]:
-            if i != self.bot_move[0]:
-                self.bot_move.remove(i)
+        self.bot_move.clear()
+        self.bot_move.append(random.choice(moves))
+        return self.bot_move[0]
 
     def bot_chater(self):
         phrases = ["HEllooo", "Whats up", "I will win", "This game is fun!"]
@@ -34,4 +25,3 @@ class Enemy:
     def bot_color(self):
         colours = ["orange", "pink", "red", "green", "black"]
         return str(random.choice(colours))
-
