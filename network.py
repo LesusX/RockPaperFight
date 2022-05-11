e64b3393ee332d541797883b7f72fcb7fffb3817
7eventh <55671529+7eventh@users.noreply.github.com>
2022-05-12 00:08:24 +0200

    Update network.py

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.154.196.62" # Home "192.168.1.75"  # School "10.154.196.89"
+        self.server = "IPV4" 
         self.port = 5556
         self.addr = (self.server, self.port)
         self.p = self.connect()
@@ -16,13 +16,13 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048*6).decode()
+            return self.client.recv(4096*6).decode()
         except:
             pass
 
     def send(self, data):
         try:
             self.client.send(str.encode(data))
-            return pickle.loads(self.client.recv(2048*6))
+            return pickle.loads(self.client.recv(4096*6))
         except socket.error as e:
-            print(e)    
+            print(e) 
