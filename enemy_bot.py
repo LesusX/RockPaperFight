0cfe614a2b58ca2f96e830327bbbb74831d11bdd
7eventh <55671529+7eventh@users.noreply.github.com>
2022-05-30 12:29:32 +0200

    Update enemy_bot.py

--- enemy_bot.py
+++ enemy_bot.py
@@ -16,21 +16,23 @@ class Enemy:
 
     # Pick a random sublcass that represents a champion and return it to the offline_game.py to create an object 
     def pick_champion(self): 
-        champions = [EnemyMeleeChampionOne]
+        champions = [EnemyMeleeChampionOne, EnemyMeleeChampionTwo, EnemyMeleeChampionThree, EnemyMeleeChampionFour, EnemyMeleeChampionFive,
+        EnemyMeleeChampionSix, EnemyMeleeChampionSeven, EnemyMeleeChampionEight, EnemyMeleeChampionNine, EnemyMeleeChampionTen, EnemyMeleeChampionEleven]
         self.enemy_champion.clear()
         self.enemy_champion.append(random.choice(champions))
         return self.enemy_champion[0]
 
-    def set_bot_move(self):
-        moves = ["Rock", "Paper", "Scissors"]
-        self.bot_move.clear()
+    def set_bot_move(self): # There should be only three choices. Right now if the list has only 3 choices then it gives the same result 4> in a row- 
+        moves = ["Rock", "Paper", "Scissors", "Rock", "Paper", "Scissors", "Paper", "Scissors"] # TODO: Fix the random choice so it does not give the same result more than 2 times in a row. 
+        self.bot_move.clear() # Always clear the list before making a new choice 
         self.bot_move.append(random.choice(moves))
         return self.bot_move[0]
 
-    def bot_chater(self):
+    # LEt the bot "talk" to the player. 
+    def bot_chater(self):   # TODO Use these function on later versions of the game 
         phrases = ["HEllooo", "Whats up", "I will win", "This game is fun!"]
         return str(random.choice(phrases))
-
+    # Set a color for the Bots avatar 
     def bot_color(self):
         colours = ["orange", "pink", "red", "green", "black"]
         return str(random.choice(colours))
