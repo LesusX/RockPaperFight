775c64894a26e7a4fe85edbd491cc88b56c6738d
7eventh <55671529+7eventh@users.noreply.github.com>
2022-05-30 11:28:45 +0200

    Update network.py

--- network.py
+++ network.py
@@ -1,25 +1,27 @@
 import socket
 import pickle
-
-
+'''
+The Network file is responsible for the comunication between the clients and the server. 
+'''
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "IPV4" 
+        self.server = "SET_IPv4_HERE" 
         self.port = 5556
         self.addr = (self.server, self.port)
         self.p = self.connect()
-
+    # Get players id 
     def getP(self):
         return self.p
-
+    # Connect to server 
     def connect(self):
         try:
             self.client.connect(self.addr)
             return self.client.recv(4096*6).decode()
         except:
             pass
-
+    # Sent/recieve all data in string form
+    # TODO: In the future let objects be sent and recieved as well.  
     def send(self, data):
         try:
             self.client.send(str.encode(data))
